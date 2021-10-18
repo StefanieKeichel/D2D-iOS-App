7e01371e4ae4e54e7916877d12a16088ab119e1c
Ahmed Eldably <ahmedeldabli@gmail.com>
2021-10-18 15:35:51 +0200

    Presenting the app title in a fancy way with the timer method

--- D2D/D2D/Controllers/WelcomeViewController.swift
+++ D2D/D2D/Controllers/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  D2D
 //
 //  Created by Ahmed Eldably on 18.10.21.
@@ -9,9 +9,24 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Initalize empty text
+        titleLabel.text = ""
+        let titleText = "D2D Communication"
+        var charIndex = 0.0
+        
+        // loop on every letter in the text and show every letter every 0.1 seconds
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) {(timer) in self.titleLabel.text?.append(letter)
+                
+            }
+            charIndex += 1
+        }
     }
 
 
