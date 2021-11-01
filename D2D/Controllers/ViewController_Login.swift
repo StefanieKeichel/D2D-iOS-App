c47a5cbeb570084355df890c3cf793f1aa5f1ce3
Stefanie Keichel <58078457+StefanieKeichel@users.noreply.github.com>
2021-11-01 14:16:47 +0100

    fix merging conflicts

---
+++ D2D/Controllers/ViewController_Login.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController_Login.swift
+//  D2D
+//
+//  Created by Stefanie Keichel on 01.11.21.
+//
+
+import UIKit
+
+class ViewController_Login: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
