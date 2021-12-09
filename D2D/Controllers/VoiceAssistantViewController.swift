92c9ada2cee367421ce7f2cd44c215175cdeb931
Stefanie Keichel <58078457+StefanieKeichel@users.noreply.github.com>
2021-12-09 19:34:48 +0100

    implementing design changes into chat feature and also transferring the voice message directly into the chat

--- D2D/Controllers/VoiceAssistantViewController.swift
+++ D2D/Controllers/VoiceAssistantViewController.swift
@@ -4,29 +4,40 @@ import InstantSearchVoiceOverlay
 class VoiceAssistant: UIViewController {
 
     let voiceOverlay = VoiceOverlayController()
+    var user_message = ""
 
     @IBOutlet weak var recording_button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBar.tintColor = UIColor(red: 251/255, green: 247/255, blue: 255/255, alpha: 1.0)
         recording_button.sendActions(for: .touchUpInside)
-        
-
     }
     @IBAction func recording_button(_ sender: UIButton) {
         
-        voiceOverlay.start(on: self, textHandler: {text, final, _ in
+        voiceOverlay.start(on: self, textHandler: { [self]text, final, _ in
             if final {
                 print("\(text)")
-//                self.navigationController?.pushViewController(Chat.storyboard, animated: true)
+                user_message = "\(text)"
                 self.performSegue(withIdentifier: "showchatstoryboard", sender: self)
             }
             else {
                 print("In progress: \(text)")
+                user_message = ""
             }
         }, errorHandler: { error in
             
         })
     }
+
+    @IBAction func GoTo_Chat(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "showchatstoryboard", sender: self)
+    }
     
+    //    Passing variables from one ViewController to the next with this function
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            let destVC = segue.destination as! ChatViewController
+            destVC.voicemessage = self.user_message
+        }
 }
+
